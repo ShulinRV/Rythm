5c438812059fca9c5695a7f237709445d39d1e31
awkwardWinkyface <barannik.r.v@gmail.com>
2022-06-30 16:40:21 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+
+    const swiper = new Swiper('.swiper-reviews', {
+        // Optional parameters
+        //direction: 'vertical',
+        autoHeight: true,
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+
+        // Navigation arrows
+        //navigation: {
+        //  nextEl: '.swiper-button-next',
+        //  prevEl: '.swiper-button-prev',
+        //},
+
+        // And if we need scrollbar
+        //scrollbar: {
+        //el: '.swiper-scrollbar',
+
+    });
+}
